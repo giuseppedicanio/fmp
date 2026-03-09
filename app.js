cbd03dd7116f2b9d951b92210fd5f074f4d909c8
giuseppedicanio <giusedicanio@gmail.com>
2026-03-09 11:38:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,59 @@
-const fileInput = document.getElementById("fileInput")
+const upload = document.getElementById("upload")
+const player = document.getElementById("player")
 const video = document.getElementById("video")
-const playBtn = document.getElementById("play")
+
+const play = document.getElementById("play")
 const progress = document.getElementById("progress")
-const time = document.getElementById("time")
+const volume = document.getElementById("volume")
+const fullscreen = document.getElementById("fullscreen")
 
-fileInput.addEventListener("change", () => {
+upload.addEventListener("change", () => {
 
-    const file = fileInput.files[0]
-
+    const file = upload.files[0]
     if(!file) return
 
     const url = URL.createObjectURL(file)
 
     video.src = url
+
+    upload.style.display = "none"
+    player.style.display = "block"
+
 })
 
-playBtn.addEventListener("click", () => {
+play.onclick = () => {
 
     if(video.paused){
         video.play()
-        playBtn.textContent = "Pause"
+        play.textContent = "Pause"
     }else{
         video.pause()
-        playBtn.textContent = "Play"
+        play.textContent = "Play"
     }
 
-})
+}
 
 video.addEventListener("timeupdate", () => {
 
     const percent = (video.currentTime / video.duration) * 100
     progress.value = percent
 
-    const minutes = Math.floor(video.currentTime / 60)
-    const seconds = Math.floor(video.currentTime % 60).toString().padStart(2,"0")
-
-    time.textContent = `${minutes}:${seconds}`
-
 })
 
 progress.addEventListener("input", () => {
 
-    const time = (progress.value / 100) * video.duration
-    video.currentTime = time
+    video.currentTime = (progress.value / 100) * video.duration
 
 })
+
+volume.addEventListener("input", () => {
+
+    video.volume = volume.value
+
+})
+
+fullscreen.onclick = () => {
+
+    video.requestFullscreen()
+
+}
